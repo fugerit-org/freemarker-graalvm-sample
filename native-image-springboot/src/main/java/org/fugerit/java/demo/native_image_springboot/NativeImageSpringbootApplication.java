725490cb7f18b043e188cec061e70fc17f519f8d
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-05-14 22:12:59 +0200

    FreeMarker native-image-springboot sample added

---
+++ native-image-springboot/src/main/java/org/fugerit/java/demo/native_image_springboot/NativeImageSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.fugerit.java.demo.native_image_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeImageSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeImageSpringbootApplication.class, args);
+	}
+
+}
---
+++ native-image-springboot/src/test/java/org/fugerit/java/demo/native_image_springboot/NativeImageSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fugerit.java.demo.native_image_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeImageSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
